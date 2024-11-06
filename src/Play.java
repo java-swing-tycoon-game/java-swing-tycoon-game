d1d95dcd02ed2b8b5fc99ead5cd748562efad7b6
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-11-06 16:53:09 +0900

    하단에 타임bar와 코인 추가

--- src/Play.java
+++ src/Play.java
@@ -10,17 +10,63 @@ public class Play extends JFrame {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1024, 768);
-
         setVisible(true);
-        pack();
     }
 
     void showBackground()
     {
-        JLabel bgImageLabel = new JLabel(new ImageIcon("assets/img/edge.png"));
-        setContentPane(bgImageLabel);
+        // 배경 이미지
+        JLabel bgImage = new JLabel(new ImageIcon("assets/img/edge.png"));
+        setContentPane(bgImage);
+        bgImage.setLayout(new BorderLayout());
 
-        JLabel mapImageLabel = new JLabel(new ImageIcon("assets/img/map.png"));
-        setContentPane(mapImageLabel);
+        // 맵 이미지
+        JPanel mapPanel = new JPanel();
+        mapPanel.setOpaque(false);
+        mapPanel.setLayout(new FlowLayout());
+        mapPanel.setSize(958,667);
+
+        JLabel mapImage = new JLabel(new ImageIcon("assets/img/map.png"));
+
+
+        // 하단 패널
+        JPanel bottom = new JPanel();
+        bottom.setOpaque(false);
+        bottom.setLayout(new BorderLayout());
+        bottom.setBorder(BorderFactory.createEmptyBorder(0 , 50, 0 , 50));
+
+        // 시간
+        JPanel timePanel = new JPanel();
+        timePanel.setOpaque(false);
+        timePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JLabel time = new JLabel(new ImageIcon("assets/img/time.png"));
+        JLabel timeBar = new JLabel(new ImageIcon("assets/img/timeBar.png"));
+
+        // 코인
+        JPanel coinPanel = new JPanel();
+        coinPanel.setOpaque(false);
+        coinPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        JLabel coin = new JLabel(new ImageIcon("assets/img/coin.png"));
+        JLabel coinImage = new JLabel(new ImageIcon("assets/img/coinImage.png"));
+        Font font = new Font("궁서", Font.BOLD, 30);
+        JLabel coinTxt = new JLabel("x 1 만원");
+        coinTxt.setFont(font);
+
+
+        mapPanel.add(mapImage);
+
+        timePanel.add(time);
+        timePanel.add(timeBar);
+        coinPanel.add(coin);
+        coinPanel.add(coinImage);
+        coinPanel.add(coinTxt);
+
+        bottom.add(timePanel, BorderLayout.WEST);
+        bottom.add(coinPanel, BorderLayout.EAST);
+
+        bgImage.add(bottom, BorderLayout.SOUTH);
+        bgImage.add(mapPanel, BorderLayout.CENTER);
     }
 }
