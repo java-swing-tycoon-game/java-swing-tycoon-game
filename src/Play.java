c744357e838895dc302052b7b63c30eb60277aad
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-11-06 15:21:56 +0900

    배경 이미지 가운데 정렬

--- src/Play.java
+++ src/Play.java
@@ -1,23 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Play extends JFrame {
     Play() {
-        setTitle("Main");
-        setSize(1024, 768);
+        setTitle("청춘 소녀는 콘서트의 꿈을 꾸지 않는다");
+
+        setLayout(new BorderLayout());
+        showBackground();
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null); // 레이아웃 널로 설정 --> 위치 지정 마음대로
-
-        ImageIcon bgImage = new ImageIcon("assets/img/edge.png"); // 배경
-        JLabel bgImageLabel = new JLabel(bgImage);
-        bgImageLabel.setBounds(0, 0, getWidth(), getHeight());
-
-        ImageIcon map = new ImageIcon("assets/img/map.png"); // 맵 이미지
-        JLabel mapImage = new JLabel(map);
-        mapImage.setBounds(0, 0, getWidth(), getHeight());
-
-        add(mapImage);
-        add(bgImageLabel);
+        setSize(1024, 768);
 
         setVisible(true);
+        pack();
+    }
+
+    void showBackground()
+    {
+        JLabel bgImageLabel = new JLabel(new ImageIcon("assets/img/edge.png"));
+        setContentPane(bgImageLabel);
+
+        JLabel mapImageLabel = new JLabel(new ImageIcon("assets/img/map.png"));
+        setContentPane(mapImageLabel);
     }
 }
