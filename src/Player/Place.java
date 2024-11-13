c95c11c9ea13ba3af180e8b29a7a4873384ce296
yyen <brenda1217@naver.com>
2024-11-13 14:41:00 +0900

    Move 수정

---
+++ src/Player/Place.java
@@ -0,0 +1,45 @@
+package Player;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Place {
+    int x, y, radius;
+    int targetX, targetY;
+
+    // Circle 클래스의 생성자
+    public Place(int x, int y, int radius, int targetX, int targetY) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.targetX = targetX;
+        this.targetY = targetY;
+    }
+
+    // 클릭한 좌표가 원 안에 포함되어 있는지 확인하는 메서드
+    public boolean contains(int mouseX, int mouseY) {
+        int dx = mouseX - x;
+        int dy = mouseY - y;
+        return dx * dx + dy * dy <= radius * radius;
+    }
+
+    // 원 객체들을 관리하는 메서드 (원 객체들의 리스트를 반환)
+    public static ArrayList<Place> createPlaces() {
+        ArrayList<Place> places = new ArrayList<>();
+        places.add(new Place((int) 72.5, (int) 437.5, (int) 22.5, (int) 90, 520));  // 아이템1
+        places.add(new Place((int) 132.5, (int) 417.5, (int) 22.5, (int) 140, 500));  // 아이템2
+        places.add(new Place((int) 192.5, (int) 397.5, (int) 22.5, (int) 200, 480));  // 아이템3
+
+        places.add(new Place((int) 842.5, (int) 397.5, (int) 22.5, 820, 480));  // 아이템4
+        places.add(new Place((int) 902.5, (int) 417.5, (int) 22.5, 880, 500));  // 아이템5
+        places.add(new Place((int) 962.5, (int) 437.5, (int) 22.5, 930, 520));  // 아이템6
+
+        places.add(new Place(285, 245, 75, 310, 260));  // 장소1
+        places.add(new Place(525, 175, 75, 550, 210));  // 장소2
+        places.add(new Place(775, 245, 75, 890, 250));  // 장소3
+
+        return places;
+    }
+}
+
+
