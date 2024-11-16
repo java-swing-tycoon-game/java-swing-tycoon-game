cb7d250e968f9fc81040906ac05d8eccce2dae79
ian <hope030327@naver.com>
2024-11-17 06:07:14 +0900

    응원봉 타임 관련

---
+++ src/Items/LightStick.java
@@ -0,0 +1,19 @@
+package Items;
+
+public class LightStick {
+    private int timeIncrease; // 응원봉 사용 시 증가하는 시간
+    private int totalTime;    // 응원봉 사용으로 누적된 총 시간
+
+    public LightStick() {
+        this.timeIncrease = 5; // 기본 증가 시간 (5초)
+        this.totalTime = 0;     // 총 시간 초기화
+    }
+
+    public int getTimeIncrease() {
+        return timeIncrease;
+    }
+
+    public void use() {
+        totalTime += timeIncrease; // 응원봉 사용 시 누적 시간
+    }
+}
