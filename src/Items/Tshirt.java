88ebddfc1810285ed175a8344a476948e6a516e8
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-12-01 02:09:57 +0900

    티셔츠 효과

--- src/Items/Tshirt.java
+++ src/Items/Tshirt.java
@@ -3,16 +3,10 @@ package Items;
 import Character.MovePlayer;
 
 public class Tshirt {
-    private static final int SPEED_BOOST = 10;
+    private static final int SPEED_BOOST = 15;
 
     public static void applyEffect(MovePlayer player) {
-        //player.fastMove();
+        player.setSpeed(SPEED_BOOST);
         System.out.println("티셔츠 효과 적용 완료.");
     }
-
-//    public void tshirtEffect(Move characterMove) {
-//        int currentSpeed = characterMove.getMoveSpeed();    // 현재 캐릭터 이동 속도
-//        characterMove.setMoveSpeed(currentSpeed + speedBoost);
-//        System.out.println("티셔츠 효과 적용.. 현재 이동 속도: " + characterMove.getMoveSpeed());
-//    }
 }
