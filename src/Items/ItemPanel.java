c5442055d672973f0a5c623c4a01fa92483a00ba
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-12-01 02:10:23 +0900

    특수효과 버튼화

--- src/Items/ItemPanel.java
+++ src/Items/ItemPanel.java
@@ -1,5 +1,8 @@
 package Items;
 
+import Scenes.Play;
+import Character.bcAns;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -16,7 +19,8 @@ public class ItemPanel extends JPanel {
     public ItemPanel() {
         setOpaque(false);
         instance = this;
-        setLayout(new FlowLayout(FlowLayout.RIGHT, 15, 0)); // 아이템 간격 설정
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0)); // 아이템 간격 설정
         refreshItems();
     }
     public void refreshItems() {
@@ -25,21 +29,59 @@ public class ItemPanel extends JPanel {
 
         itemArray[0] = !(itemArray[1] || itemArray[2] || itemArray[3]);
 
+        // 티셔츠 아이템 자동 클릭
+        if (itemArray[2]) {
+            onItemClicked(2);
+        }
+
         for (int i = 0; i < itemArray.length; i++) {
             if (itemArray[i]) {
-                JLabel itemLabel = new JLabel(new ImageIcon(itemIcons[i]));
-                add(itemLabel);
+                add(createItemButton(i));
             }
         }
         revalidate();
         repaint();
     }
 
-    private void addDefaultItem() {
-        JLabel defaultItemLabel = new JLabel(new ImageIcon("assets/img/item.png"));
-        add(defaultItemLabel);
+    private JButton createItemButton(int index) {
+        JButton itemButton = new JButton(new ImageIcon(itemIcons[index])); // 아이템 이미지를 버튼으로
+        itemButton.setContentAreaFilled(false); // 버튼 배경 제거
+        itemButton.setBorderPainted(false); // 버튼 테두리 제거
+        itemButton.setFocusPainted(false); // 포커스 표시 제거
+        itemButton.setToolTipText("아이템 " + (index + 1));
+
+        // 버튼 클릭 이벤트 추가
+        itemButton.addActionListener(e -> onItemClicked(index));
+        return itemButton;
     }
 
+    private void onItemClicked(int index) {
+        // 클릭된 아이템에 대한 로직 추가
+        if (index == 1) {
+            System.out.println("슬로건 아이템 활성화!");
+            bcAns.stop();
+            itemArray[1] = false;
+        } else if (index == 2) {
+            System.out.println("티셔츠 아이템 활성화!");
+            Tshirt.applyEffect(Play.player);
+        } else if (index == 3) {
+            System.out.println("스틱 아이템 활성화!");
+            LightStick.use();
+            itemArray[3] = false;
+        }
 
+        //refreshItems();
+    }
 
+    private void addDefaultItem() {
+        JButton defaultItemButton = new JButton(new ImageIcon("assets/img/item.png"));
+        defaultItemButton.setContentAreaFilled(false);
+        defaultItemButton.setBorderPainted(false);
+        defaultItemButton.setFocusPainted(false);
+        defaultItemButton.setToolTipText("기본 아이템");
+
+        // 기본 아이템 클릭 이벤트
+        defaultItemButton.addActionListener(e -> System.out.println("기본 아이템 클릭됨!"));
+        add(defaultItemButton);
+    }
 }
