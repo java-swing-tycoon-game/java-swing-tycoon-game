05da7d91e33bf3a0f9e1874fea4b4b2880234ecf
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-12-02 04:59:29 +0900

    이속 조정

--- src/Items/Tshirt.java
+++ src/Items/Tshirt.java
@@ -1,12 +1,16 @@
 package Items;
 
 import Character.MovePlayer;
+import Scenes.Play;
+
+import javax.swing.*;
 
 public class Tshirt {
-    private static final int SPEED_BOOST = 15;
+    private static final int SPEED_BOOST = 10;
 
     public static void applyEffect(MovePlayer player) {
         player.setSpeed(SPEED_BOOST);
         System.out.println("티셔츠 효과 적용 완료.");
+        Play.player.imageChange("assets/img/playerCharacter_fast.png");
     }
 }
